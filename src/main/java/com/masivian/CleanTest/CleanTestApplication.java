5806875652a5a1644cdc5536c6c22f177b6f14b3
VivianaLeyva <viviana.leyva.dcsas@gmail.com>
2020-06-01 20:11:02 -0500

    initial commit

---
+++ src/main/java/com/masivian/CleanTest/CleanTestApplication.java
@@ -0,0 +1,13 @@
+package com.masivian.CleanTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanTestApplication.class, args);
+	}
+
+}
